06ebf874d772e234f645d0fdb00964e9b717def4
musman4 <musman4@uic.edu>
2022-01-24 14:09:18 -0600

    first commit

--- hw1_syscall.c
+++ hw1_syscall.c
@@ -3,7 +3,7 @@
 int main() {
   register int    syscall_no  asm("rax") = 1;
   register int    arg1        asm("rdi") = 1;
-  register char*  arg2        asm("rsi") = "Hello, World!\n";
+  register char*  arg2        asm("rsi") = "Hello, world!\n";
   register int    arg3        asm("rdx") = 14;
   asm("syscall");
 }
